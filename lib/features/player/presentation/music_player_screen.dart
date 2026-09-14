79de9593cde8731d20ff92baa64b407f6a8fd809
hoanhanh1102 <duyho4369@gmail.com>
2026-09-15 00:12:43 +0700

    fix

--- lib/features/player/presentation/music_player_screen.dart
+++ lib/features/player/presentation/music_player_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:math' as math;
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide RepeatMode;
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/constants/app_colors.dart';
--- lib/features/player/presentation/video_player_screen.dart
+++ lib/features/player/presentation/video_player_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:math' as math;
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide RepeatMode;
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:video_player/video_player.dart';
